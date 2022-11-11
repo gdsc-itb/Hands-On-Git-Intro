53e23db6baa3fff5be3897f074a862d8c0ab103e
FrancescoMichael <francescokusuma15@gmail.com>
2022-11-11 22:06:04 +0700

    First submission by Francesco Michael Kusuma

---
+++ Francesco Michael_ITB/main.cpp
@@ -0,0 +1,18 @@
+#ifndef LOCAL
+#pragma GCC optimize ("Ofast")
+#pragma GCC optimize ("unroll-loops")
+#endif
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+    cout << "Nama : Francesco Michael Kusuma\n";
+    cout << "Jurusan : STEI-Komputasi\n";
+    cout << "Universitas : Institut Teknologi Bandung\n";
+    cout << "Alasan ingin masuk GDSC : Ingin menambah ilmu dan pengalaman yang saya miliki.";
+
+    return 0;
+}
