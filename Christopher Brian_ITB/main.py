39bf4d85517d705e581b828e69e335ab4f7e30ca
ChristopherBrian <cbnnolan@gmail.com>
2022-11-11 22:42:50 +0700

    First Submission by Christopher Brian

---
+++ Christopher Brian_ITB/main.py
@@ -0,0 +1,4 @@
+print("Nama: Christopher Brian")
+print("Jurusan: TPB STEI-K")
+print("Universitas: ITB")
+print("Alasan ingin masuk GDSC: Menambah ilmu dan pengalaman, memperluas koneksi, berkontribusi terhadap masyarakat")
