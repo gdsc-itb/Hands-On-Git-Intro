38f9913287e55729595db56036f43c9ee0e35880
zultopia <117258762+zultopia@users.noreply.github.com>
2022-11-11 23:05:57 +0700

    First submission by Marzuli Suhada M

---
+++ Marzuli Suhada M_ITB/main.py
@@ -0,0 +1,4 @@
+print("Nama: Marzuli Suhada M")
+print("Jurusan: TPB STEI-K")
+print("Universitas: ITB")
+print("Alasan ingin masuk GDSC: Karena saya yakin ilmu dan pengalaman yang didapat akan berguna untuk karier di masa depan ")
