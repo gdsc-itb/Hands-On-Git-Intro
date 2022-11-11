a1280b53cc3026e2d32e80f93ddb12a270a6c755
Jesslyn Audrey <jesslynaudreyy@gmail.com>
2022-11-11 22:22:43 +0700

    First submission by Jesslyn Audrey

--- Jesslyn Audrey_BINUS/Identitas.md
+++ Jesslyn Audrey_BINUS/Identitas.md
@@ -1,5 +1,4 @@
 # First submission by Malik Akbar
 
 ## Screenshot
-![Screenshot](screenshot.png)
 ![Screenshot](Screenshot 2022-11-11 Jesslyn Audrey.png)
---
+++ Jesslyn Audrey_BINUS/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public Main() {
+        System.out.println("Nama: Jesslyn Audrey");
+        System.out.println("Jurusan: Computer Science");
+        System.out.println("Universitas: Bina Nusantara");
+        System.out.println("Alasan ingin masuk GDSC: Ingin memiliki pengetahuan yang lebih luas terkait pemrograman dengan teman-teman seperjuangan lainnya!");
+    }
+    
+    public static void main(String[] args) {
+        new Main();
+    }
+}
