db19da800b438e6a826ad8c7d795c714d0c782ad
FarelW9 <winaldafarell@gmail.com>
2022-11-11 22:57:39 +0700

    First submission by Farel Winalda

---
+++ Farel Winalda_ITB/main.py
@@ -0,0 +1,4 @@
+print("Nama: Farel Winalda")
+print("Jurusan: STEI-K")
+print("Universitas: ITB")
+print("Alasan ingin masuk GDSC: Ingin belajar lebih banyak tentang pemrograman dan mengetahui lebih banyak tentang google")
