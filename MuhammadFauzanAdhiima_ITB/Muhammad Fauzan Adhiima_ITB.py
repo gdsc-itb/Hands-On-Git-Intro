4662b5d4fe8561835d96cdde73e35141e7d17ab2
1magines <16022433@mahasiswa.itb.ac.id>
2022-11-11 22:03:27 +0700

    First Submission by Muhammad Fauzan Adhiima

---
+++ MuhammadFauzanAdhiima_ITB/Muhammad Fauzan Adhiima_ITB.py
@@ -0,0 +1,8 @@
+print("Nama: Muhammad Fauzan Adhiima")
+print("Jurusan: FMIPA-IPA")
+print("Universitas: ITB")
+print(
+    "Alasan ingin masuk GDSC: Ingin mempelajari lebih lanjut mengenai programming," "\n"
+    "membuat inovasi dalam teknologi serta berkolaborasi dengan sesama member GDSC" "\n"
+    "ITB untuk membuat Inovasi yang dapat memajukan perkembangan teknologi" "\n"
+)
