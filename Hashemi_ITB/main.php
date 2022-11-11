0347b2d61a48f7ee36fff5b330924e6bdf23dea4
Malik Rafsanjani <pro.malikakbar2357@gmail.com>
2022-11-11 21:10:39 +0700

    First submission by hashemi

---
+++ Hashemi_ITB/main.php
@@ -0,0 +1,6 @@
+<?php
+
+echo "Nama: Malik Akbar" . PHP_EOL;
+echo "Jurusan: Teknik Informatika" . PHP_EOL;
+echo "Universitas: ITB" . PHP_EOL;
+echo "Alasan ingin masuk GDSC: Ingin belajar lebih banyak tentang pemrograman" . PHP_EOL;
