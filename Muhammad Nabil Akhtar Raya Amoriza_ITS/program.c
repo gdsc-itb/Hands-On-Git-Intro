49dff24938d71ee3b4312d22f7155ba69a72f26c
reivertt <billthehuman27@gmail.com>
2022-11-12 01:54:50 +0700

    First submission by Muhammad Nabil Akhtar Raya Amoriza

---
+++ Muhammad Nabil Akhtar Raya Amoriza_ITS/program.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(void){
+    printf("Nama : Muhammad Nabil Akhtar Raya Amoriza\n");
+    printf("Jurusan : Teknik Informatika\n");
+    printf("Universitas : Institut Teknologi Sepuluh Nopember\n");
+    printf("Alasan ingin masuk GDSC : Agar dapat memperluas pengetahuan saya dan memajukan bangsa dengan pengetahuan tersebut.");
+}
