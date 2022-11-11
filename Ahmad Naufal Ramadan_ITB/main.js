55c8c71ae60760a7ebe9c9f04e9db5913ba0a54c
naufal <naufalahmad022@gmail.com>
2022-11-11 22:32:36 +0700

    First submission by Ahmad Naufal Ramadan

---
+++ Ahmad Naufal Ramadan_ITB/main.js
@@ -0,0 +1,6 @@
+console.log("Nama: Ahmad Naufal Ramadan");
+console.log("Jurusan: STEI-K");
+console.log("Universitas: ITB");
+console.log(
+  "Alasan ingin masuk GDSC: Alasanku ingin menjadi member GDSC ITB adalah ingin menambah menambah wawasan mengenai hal hal yang berkaitan dengan software engineering dan juga menambah pengalaman dan relasi."
+);
