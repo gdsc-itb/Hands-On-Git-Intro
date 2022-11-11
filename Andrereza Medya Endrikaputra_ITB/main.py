2dfc62a397402e39bf23aa871cf36d83b4f5ede6
andrerezaatik@gmail.com <andrerezaatik@gmail.com>
2022-11-11 22:56:51 +0700

    First submission by Andrereza Medya Endrikaputra

---
+++ Andrereza Medya Endrikaputra_ITB/main.py
@@ -0,0 +1,4 @@
+print("Nama: Andrereza Medya Endrikaputra")
+print("Jurusan: TPB-FITB")
+print("Universitas: ITB")
+print("Alasan ingin masuk GDSC: Ingin meningkatkan skill programming dan mencari jati diri")
