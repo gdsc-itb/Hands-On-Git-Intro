c0081c765c67c650dba8b45ffa7c621131fbf14e
PanjiSri <neiabaraja04@gmail.com>
2022-11-11 22:34:07 +0700

    First submission by Panji Sri Kuncara Wisma

---
+++ Panji Sri Kuncara Wisma_ITB/main.py
@@ -0,0 +1,4 @@
+print("Nama: Panji Sri Kuncara Wisma")
+print("Jurusan: TPB Sekolah Teknik Elektro dan Informatika Komputasi")
+print("Universitas: ITB")
+print("Alasan ingin masuk GDSC: Ingin belajar lebih banyak tentang pemrograman dan memperbanyak pengalaman")
