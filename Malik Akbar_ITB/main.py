b9ed603cec9c56460c37477d319b0f62937db634
JonathanFelix03 <jonathan.felixchan@gmail.com>
2022-11-11 22:18:31 +0700

    test1

--- Malik Akbar_ITB/main.py
+++ Malik Akbar_ITB/main.py
@@ -1,4 +1,4 @@
-print("Nama: Malik Akbar")
-print("Jurusan: Teknik Informatika")
-print("Universitas: ITB")
-print("Alasan ingin masuk GDSC: Ingin belajar lebih banyak tentang pemrograman")
+print("Nama:Jonathan Felix")
+print("Jurusan: Sistem Informasi")
+print("Universitas: Binus University")
+print("Alasan ingin masuk GDSC: Ingin mempelajari web development dan mengenal pemograman lebih jauh")
