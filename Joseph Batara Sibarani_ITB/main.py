3151e294a34fd43fd45a3c771c5e9a817b27ddbb
Joseph <josephsibarani@outlook.com>
2022-11-11 22:22:11 +0700

    First submission by Joseph Batara Sibarani

---
+++ Joseph Batara Sibarani_ITB/main.py
@@ -0,0 +1,4 @@
+print ("Nama : Joseph Batara Sibarani")
+print ("Jurusan : TPB")
+print ("Universitas : ITB")
+print ("Alasan ingin masuk GDSC : Ingin belajar tentang coding dan yang beruhubungan tentang teknologi")
