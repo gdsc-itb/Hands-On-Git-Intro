d525f4e60d044de0fdfbe22207a2c8b70f115bb3
Neurologia <patrickkwon777@gmail.com>
2022-11-11 22:00:08 +0700

    First submission by Patrick Kwon

---
+++ Patrick_Kwon_Binus/main.go
@@ -0,0 +1,12 @@
+package main
+
+import(
+	"fmt"
+)
+func main(){
+	fmt.Println("Nama: Patrick Kwon")
+	fmt.Println("Jurusan: Computer Science")
+	fmt.Println("Universitas: Bina Nusantara")
+	fmt.Println("Alasan Ingin Masuk GDSC: Membangun koneksi dan memperluas wawasan")
+
+}
