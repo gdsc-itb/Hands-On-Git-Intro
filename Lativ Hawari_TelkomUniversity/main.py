571d15a38265bb5d63c6ee90bf3a1fcd86022e0d
lativ <lativhwr06@gmail.com>
2022-11-11 22:42:12 +0700

    First submission by Lativ Hawari

---
+++ Lativ Hawari_TelkomUniversity/main.py
@@ -0,0 +1,4 @@
+print("Nama: Lativ Hawari")
+print("Jurusan: Teknik Industri")
+print("Universitas: Universitas Telkom")
+print("Alasan ingin masuk GDSC: Ingin belajar lebih banyak tentang pemrograman")
