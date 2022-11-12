e05942af58fbb8fc6a9811772662ff0225536f6f
debrinashika <118027372+debrinashika@users.noreply.github.com>
2022-11-12 07:33:52 +0700

    Delete main.py

--- main.py
+++
@@ -1,4 +0,0 @@
-print("Nama: Debrina Veisha Rashika")
-print("Jurusan: STEI Komputasi")
-print("Universitas: ITB")
-print("Alasan ingin masuk GDSC: Ingin belajar lebih banyak tentang pemrograman dan software development serta menambah relasi")
