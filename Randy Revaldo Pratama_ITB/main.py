d57959987704b80eb6db69befece056d6941b7a4
randyyyyy25 <randyrevaldo64@gmail.com>
2022-11-12 22:10:10 +0700

    First submission by Randy Revaldo Pratama

---
+++ Randy Revaldo Pratama_ITB/main.py
@@ -0,0 +1,4 @@
+print("Nama: Randy Revaldo Pratama")
+print("Jurusan: STEI-R")
+print("Universitas: ITB")
+print("Alasan ingin masuk GDSC: Ingin belajar lebih banyak tentang pemrograman, serta mencari teman yang memiliki minat yang sama")
