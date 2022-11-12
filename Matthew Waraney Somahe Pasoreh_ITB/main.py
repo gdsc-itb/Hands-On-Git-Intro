ba46afe833de15918c5d397c7343d523fab52709
pasorehmatthew <pasorehmatthew2003@gmail.com>
2022-11-12 13:07:56 +0700

    First submission by Matthew Waraney Somahe Pasoreh

---
+++ Matthew Waraney Somahe Pasoreh_ITB/main.py
@@ -0,0 +1,4 @@
+print("Nama: Matthew Waraney Somahe Pasoreh")
+print("Jurusan: FMIPA IPA")
+print("Universitas: ITB")
+print("Alasan ingin masuk GDSC: Saya ingin belajar mengenai programming, saya juga ingin belajar spesifik pada bidang data science dan analysis sesuai yang saya ketahui bahwa hal yang saya sebut akan berguna di fakultas saya")
