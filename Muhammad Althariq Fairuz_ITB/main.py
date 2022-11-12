577f8869e822bad04726e3813ef87a107890f866
AlthariqFairuz <althariqfairuz273@gmail.com>
2022-11-12 18:35:25 +0700

    First submission by Muhammad Althariq Fairuz

--- Muhammad Althariq Fairuz_ITB/main.py
+++ Muhammad Althariq Fairuz_ITB/main.py
@@ -1,4 +1,4 @@
 print("Nama: Muhammad Althariq Fairuz")
 print("Jurusan: TPB STEI-K")
 print("Universitas: ITB")
-print("Alasan ingin masuk GDSC: Ingin mengembangkan relasi serta memperdalam ilmu yang telah dipelajari")
+print("Alasan ingin masuk GDSC: Ingin mengembangkan relasi serta memperdalam ilmu yang telah dipelajari ")
