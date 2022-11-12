ff79efe8bbe6e74864017ae2d1b303a5318feb22
Timothy Theophilus H <89094785+timoothy21@users.noreply.github.com>
2022-11-12 19:25:00 +0700

    First submission by Timothy

---
+++ Timothy Theophilus H_BINUS/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Nama: Timothy Theophilus Hartono\n");
+    printf("Jurusan: Computer Science\n");
+    printf("Universitas: Bina Nusantara University\n");
+    printf("Alasan ingin masuk GDSC: menambah koneksi, menambah pengalaman, menambah ilmu dan pengembangan diri\n");
+    return 0;
+}
