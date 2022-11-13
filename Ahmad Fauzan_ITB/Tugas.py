01ba3c369573d86c2e1befa44ef954c5e05cf71f
tuktukgithub <ahmadfauzan2003311@gmail.com>
2022-11-13 10:30:25 +0700

    First submission by Ahmad Fauzan

---
+++ Ahmad Fauzan_ITB/Tugas.py
@@ -0,0 +1,4 @@
+print('Nama : Ahmad Fauzan')
+print('Jurusan : TPB FTTM')
+print('Universitas: ITB')
+print('Alasan ingin masuk GDSC : saya ingin masuk GDSC karena saya ingin menambah wawasan saya dalam IT dan bisa bersaing pada industry 4.0 nantinya')
