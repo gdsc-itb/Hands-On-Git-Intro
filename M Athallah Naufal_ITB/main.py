afcc180a1d2b1946585137c05780592f6deaffbc
naufal <mathnaufal@gmail.com>
2022-11-13 12:40:32 +0700

    First submission by M Athallah Naufal

---
+++ M Athallah Naufal_ITB/main.py
@@ -0,0 +1,4 @@
+print("Nama: Muhammad Athallah Naufal")
+print("Jurusan: Fakultas Teknik Mesin dan Dirgantara")
+print("Universitas: Institut Teknologi Bandung")
+print("Alasan ingin masuk GDSC: Karena ingin mendalami programming dan ingin mendapatkan teman-teman baru yang mempunyai hobi yang sama.")
