bac76811109730e34db303c888c9deef9de0a6f8
Richo Yudha Kurniawan <richoyudha027@gmail.com>
2022-11-13 17:30:42 +0700

    First Submission by Richo Yudha Kurniawan

---
+++ Richo Yudha Kurniawan_ITS/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"Nama : Richo Yudha Kurniawan"<< endl;
+    cout<<"Jurusan : Teknik Informatika"<< endl;
+    cout<<"Universitas : ITS"<< endl;
+    cout<<"Alasan : Ingin belajar banyak tentang programming"<< endl;
+    return 0;
+}
