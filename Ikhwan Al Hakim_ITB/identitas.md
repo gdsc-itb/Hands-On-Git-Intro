c2b5ad5ec5e65e5afd648f6ce14f9740f2ccd6df
Nerg <mas.ikhwanalhakim@gmail.com>
2022-11-13 08:00:57 +0700

    First submission by Ikhwan Al Hakim

---
+++ Ikhwan Al Hakim_ITB/identitas.md
@@ -0,0 +1,4 @@
+# First submission by Ikhwan Al Hakim
+
+## Screenshot
+![Screenshot](screenshot.png)
---
+++ Ikhwan Al Hakim_ITB/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Nama: Ikhwan Al Hakim" << endl;
+	cout << "Jurusan: TPB STEI-K" << endl;
+	cout << "Universitas: ITB" << endl;
+	cout << "Alasan ingin masuk GDSC: Ingin meningkatkan pengetahuan dan kemampuan mengenai teknologi" << endl;
+}
