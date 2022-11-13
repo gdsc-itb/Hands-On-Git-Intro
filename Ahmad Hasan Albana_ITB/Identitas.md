f2f7502d5852f0f6d731a0d38f4dd2dcd6b3a9b0
Bana-man <ahmadalbana98@gmail.com>
2022-11-13 16:06:02 +0700

    First submission by < Ahmad Hasan Albana >

---
+++ Ahmad Hasan Albana_ITB/Identitas.md
@@ -0,0 +1,4 @@
+# First submission by Ahmad Hasan Albana
+
+## Screenshot
+![Screenshot](screenshot.png)
---
+++ Ahmad Hasan Albana_ITB/main.py
@@ -0,0 +1,4 @@
+print("Nama: Ahmad Hasan Albana")
+print("Fakultas: STEI-K")
+print("Universitas: ITB")
+print("Alasan ingin masuk GDSC: Ingin belajar lebih banyak tentang pemrograman")
