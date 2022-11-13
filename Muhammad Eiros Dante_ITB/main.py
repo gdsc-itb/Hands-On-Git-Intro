14dcb5d07eb738e55bca62e5c4001c0d12ed8016
Dante-ME <danteeiros@gmail.com>
2022-11-13 13:45:54 +0700

    First submission by Muhammad Eiros Dante

---
+++ Muhammad Eiros Dante_ITB/main.py
@@ -0,0 +1,4 @@
+print("Nama: Muhammad Eiros Dante")
+print("Jurusan: STEI R - TPB")
+print("Universitas: ITB")
+print("Alasan ingin masuk GDSC: Ingin meningkatkan dan mendalami pengetahuan pemrograman")
