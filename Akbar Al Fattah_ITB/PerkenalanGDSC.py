64f84267445c24aabb2aa1f4e60844eef0488b34
DeltDev <deltenc88@gmail.com>
2022-11-13 21:21:12 +0700

    First submission by Akbar Al Fattah

---
+++ Akbar Al Fattah_ITB/PerkenalanGDSC.py
@@ -0,0 +1,4 @@
+print("Nama : Akbar Al Fattah")
+print("Jurusan : TPB STEI-K")
+print("Universitas : Institut Teknologi Bandung")
+print("Alasan ingin masuk GDSC : Saya ingin menambah pengalaman dan insight dalam dunia programming serta meningkatkan kemampuan bekerja sama dalam tim")
