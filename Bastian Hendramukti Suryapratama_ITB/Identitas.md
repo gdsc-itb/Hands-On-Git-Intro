3dc95a719c9c2abcfc19d81a5a412f4bc186b9b8
bumi <108353289+bumiterra@users.noreply.github.com>
2022-11-14 06:16:33 +0700

    First submission by Bastian Hendramukti Suryapratama

---
+++ Bastian Hendramukti Suryapratama_ITB/Identitas.md
@@ -0,0 +1,4 @@
+# First submission by Bastian Hendramukti Suryapratama
+
+## Screenshot
+![Screenshot](Screenshot 2022-11-14 060416.jpg)
---
+++ Bastian Hendramukti Suryapratama_ITB/main.py
@@ -0,0 +1,6 @@
+print("Nama: Bastian Hendramukti Suryapratama")
+print("Jurusan: STEI-K TPB")
+print("Universitas: ITB")
+print('''Alasan ingin masuk GDSC:
+Ingin bertemu dengan orang-orang yang memiliki ketertarikan di bidang software development
+serta belajar dari orang-orang yang lebih berpengalaman di bidang tersebut''')
