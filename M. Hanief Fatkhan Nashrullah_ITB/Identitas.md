6aeb393226517beadf24df36392b3c2e8e0da7c3
hannnoobz <hanief23fatkhan@gmail.com>
2022-11-13 21:42:58 +0700

    First submission by M. Hanief Fatkhan Nashrullah

---
+++ M. Hanief Fatkhan Nashrullah_ITB/Identitas.md
@@ -0,0 +1,4 @@
+# First submission by M. Hanief Fatkhan Nashrullah
+
+## Screenshot
+![Screenshot](screenshot.png)
---
+++ M. Hanief Fatkhan Nashrullah_ITB/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Nama: M. Hanief Fatkhan Nashrullah\n";
+    cout << "Jurusan: TPB STEI\n";
+    cout << "Universitas: ITB\n";
+    cout << "Alasan ingin masuk GDSC: Ingin mewujudkan ide menjadi sebuah project\n";
+}
