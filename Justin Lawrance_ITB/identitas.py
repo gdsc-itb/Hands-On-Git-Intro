66b897e4f0f92c2975cd93fe9226934e0eabb02a
jstnlwrn <justinlawrance18@gmail.com>
2022-11-14 00:57:33 +0700

    First submission by Justin Lawrance

---
+++ Justin Lawrance_ITB/identitas.py
@@ -0,0 +1,4 @@
+print('Nama: Justin Lawrance')
+print('Jurusan: STEI K')
+print('Universitas: Institut Teknologi Bandung')
+print('Alasan ingin masuk GDSC: ingin mengetahui lebih lanjut sekitar pemrograman')
