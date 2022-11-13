7ae847634179b176a647afb530dce2c9b858c8fc
Hifzhi Dinullah <hifzhidinullah@gmail.com>
2022-11-13 15:05:51 +0700

    First submission by Hifzhi Dinullah

---
+++ Hifzhi Dinullah_ITB/main.py
@@ -0,0 +1,4 @@
+print("Nama : Hifzhi Dinullah")
+print("Jurusan : TPB STEI Rekayasa")
+print("Universitas : ITB")
+print("Alasan ingin masuk GDSC : Saya ingin meningkatkan hardskill dan softskill saya dalam dunia teknologi terutama mengenai komputasi dan saya juga ingin dapat dipertemukan dan berkolaborasi dengan orang orang hebat dan memiliki ketertarikan yang sama dengan saya")
