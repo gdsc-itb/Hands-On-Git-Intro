11f2cf9041fd41f1e24a96524fb80d3fb2baa703
Richo Yudha Kurniawan <richoyudha027@gmail.com>
2022-11-14 09:29:43 +0700

    First submission by Richo Yudha Kurniawan

---
+++ Richo Yudha Kurniawan_ITS/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    printf("Nama : Richo Yudha Kurniawan\n");
+    printf("Jurusan : Teknik Informatika\n");
+    printf("Universitas : Institut Teknologi Sepuluh Nopember\n");
+    printf("Alasan ingin masuk GDSC : Ingin belajar pemrograman");
+    return 0;
+}
