e5eb44914c2280cc0dc997a08cd12cdc12cc043a
Pradipta Arya Daniswara <38103304+SomeRandomDolphin@users.noreply.github.com>
2022-11-14 09:40:17 +0700

    First submission by Pradipta Arya Daniswara

---
+++ Pradipta Arya Daniswara_ITS/main.py
@@ -0,0 +1,4 @@
+print("Nama: Pradipta Arya Daniswara")
+print("Jurusan: Teknik Informatika")
+print("Universitas: ITS")
+print("Alasan ingin masuk GDSC: Ingin bertemu dengan teman yang sepemikiran")
