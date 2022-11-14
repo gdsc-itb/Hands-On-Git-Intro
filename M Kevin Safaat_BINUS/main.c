12b2dcd5f3a85ceb81cd452d3607129748a4fb3e
Muhammad Kevin Safaat <kevinsafaat007@gmail.com>
2022-11-14 20:15:55 +0700

    First Submission by Muhammad Kevin Safaat

---
+++ M Kevin Safaat_BINUS/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("Nama: Muhammad Kevin Safaat\n");
+    printf("Jurusan: Computer Science\n");
+    printf("Universitas: Bina Nusantara University\n");
+    printf("Alasan ingin masuk GDSC: Saya ingin menambah, memperluas\n"); 
+    printf("dan memperdalam ilmu informasi teknologi yang di mana seiring berjalan waktu, teknologi berkembang semakin pesat dan ruang dimana kita bisa belajar semakin luas");
+
+    return 0;
+}
