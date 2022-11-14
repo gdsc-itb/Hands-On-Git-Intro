6da946a45d3612612a8c48299b85e367327830f8
pkmntrainerfrost <smaarif.achsien@gmail.com>
2022-11-14 21:29:24 +0700

    First submission by Salman Ma'arif Achsien

---
+++ Salman Ma'arif Achsien_ITB/main.py
@@ -0,0 +1,4 @@
+print("Nama : Salman Ma'arif Achsien")
+print("Jurusan : Sistem dan Teknologi Informasi")
+print("Universitas : Institut Teknologi Bandung")
+print("Alasan ingin masuk GDSC : Ingin mempelajari data analysis dan machine learning melalui kurikulumnya yang disediakan pada tahun ini, karena insyaAllah akan berkarir di bidang tersebut.")
