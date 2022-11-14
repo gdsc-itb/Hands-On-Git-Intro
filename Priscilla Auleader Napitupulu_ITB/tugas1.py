ebf1ef96b11869d1e9f8602ca237fe402eaadfc1
priscillaanpt <priscillanapitupuluu@gmail.com>
2022-11-14 19:26:20 +0700

    First submission by Priscilla Auleader Napitupulu

---
+++ Priscilla Auleader Napitupulu_ITB/tugas1.py
@@ -0,0 +1,4 @@
+print("Nama : Priscilla Auleader Napitupulu")
+print("Jurusan : Sistem Teknologi Informasi")
+print("Universitas : Institut Teknologi Bandung")
+print("Alasan ingin masuk GDSC : Karena saya ingin belajar data lebih dalam lagi")
