091f922f4436a4b0d3b34eea4e32d5c04fcdd8a1
Kharris-Khisunica <kharriskhisunica07@gmail.com>
2022-11-15 14:41:10 +0700

    First Submission by Kharris Khisunica

---
+++ Kharris Khisunica_ITB/Untitled-1.py
@@ -0,0 +1,10 @@
+print('\n')
+print("Nama                   : Kharris Khisunica")
+
+print("Jurusan                : STEI-K/TPB")
+
+print("Universitas            : Institut Teknologi Bandung")
+print("Alasan ingin masuk GDSC: Ingin belajar lebih tentang pemograman dan ingin berkembang dan menjadi ahli dalam")
+print("                         bidang teknologi terutama dalam area data science dan machine learning")
+
+print('\n')
