02ff1c2c5713ce9bd78bcfd92380a487f65c6a14
Muhammad Alfariza Rasendria <rasendria.alfariza18@gmail.com>
2022-11-15 14:28:17 +0700

    First submission by Muhammad Alfariza Rasendria

---
+++ Muhammad_Alfariza_Rasendria_Unair/main.js
@@ -0,0 +1,4 @@
+console.log("Nama: Muhammad Alfariza Rasendria");
+console.log("Jurusan: Sistem Informasi");
+console.log("Universitas: Universitas Airlangga");
+console.log("Alasan ingin masuk GDSC: Berkeinginan untuk memperluas koneksi serta ingin mengasah tentang skill pemrograman");
