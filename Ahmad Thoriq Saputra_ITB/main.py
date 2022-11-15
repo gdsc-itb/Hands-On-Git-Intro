3bff2710e1af9994f6c291d56a142e80353147dd
Thoriq <saputrathoriq@gmail.com>
2022-11-15 09:39:39 +0700

    First submission by Ahmad Thoriq Saptra

---
+++ Ahmad Thoriq Saputra_ITB/main.py
@@ -0,0 +1,7 @@
+print("""Nama: Ahmad Thoriq Saputra
+Jurusan: TPB
+Universitas: ITB
+ALasan ingin masuk GDSC: Saya ingin bergabung komunitas yang akan membantu menggapai 
+cita² saya menjadi bagian dalam Google dan belajar lebih dalam tentang what it takes
+to be a software engineering.
+""")
