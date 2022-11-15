1a4ce7d8eee5372f56942e3711cd1cda87b16509
XAVAX ​ <mdavaf17@gmail.com>
2022-11-15 10:43:41 +0700

    First submission by Muhammad Dava Fathurrahman

--- Muhammad Dava Fathurrahman_ITB/main.py
+++ Muhammad Dava Fathurrahman_ITB/main.py
@@ -1,4 +1,4 @@
 print("Nama: Muhammad Dava Fathurrahman")
 print("Jurusan: STEI-K")
 print("Universitas: ITB")
-print("Alasan ingin masuk GDSC: Ingin belajar DS/ML dan memperbanyak relasi")
+print("Alasan ingin masuk GDSC: Ingin belajar DS/ML dan memperbanyak relasi.")
