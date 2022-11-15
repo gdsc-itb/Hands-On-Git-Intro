6496ef0bd63738b5713769bba33d4201118dbe4e
DerwinRustanly <117006907+DerwinRustanly@users.noreply.github.com>
2022-11-15 22:19:16 +0700

    Delete Derwin_ITB directory

--- Derwin_ITB/Identitas.md
+++
@@ -1,4 +0,0 @@
-# First submission by Derwin Rustanly
-
-# Screenshot
-![Screenshot](screenshot_py.jpg)
--- Derwin_ITB/git1.py
+++
@@ -1,4 +0,0 @@
-print("Nama : Derwin Rustanly")
-print("Fakultas : STEI Komputasi")
-print("Universitas : ITB")
-print("Alasan masuk ITB : Ingin mengeksplor lebih jauh mengenai programming dan berkolaborasi dengan teman-teman dengan minat yang sejenis")
