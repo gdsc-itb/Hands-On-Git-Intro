1aa4bf4d5bad9e15ae2e3199a464e8d1edc8c15e
19622184 <116003508+19622184@users.noreply.github.com>
2022-11-15 21:14:35 +0700

    File latihan

---
+++ Scifo_ITB/main.py
@@ -0,0 +1,4 @@
+print("Nama : Marvin Scifo Yehezkiel Hutahaean")
+print("Jurusan : STEI-K")
+print("Universitas : Institut Teknologi Bandung")
+print("Alasan ingin masuk GDSC : Ingin belajar web development")
