fdd3f91af5b8268797297abffa2a0ab3c7744250
saadabha <saadabdulhakim08@gmail.com>
2022-11-15 10:56:04 +0700

    First submission by Sa'ad Abdul Hakim

---
+++ Saad_ITB/main.py
@@ -0,0 +1,4 @@
+print("Nama : Sa'ad Abdul Hakim")
+print("Jurusan : STEI-K")
+print("Universitas : ITB")
+print("Alasan ingin masuk GDSC : Ingin belajar tentang AI, Machine Learning, Cloud Computing, dan sebagainya serta memperluas koneksi dengan orang yang memiliki minat yang sama")
