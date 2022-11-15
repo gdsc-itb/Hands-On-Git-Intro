ad1bec797f3d5d0e7c6fe005e141525427e47a42
rizqikapratamaa <rizqikapratamaa@gmail.com>
2022-11-15 09:02:45 +0700

    First submission by Rizqika Mulia Pratama

---
+++ Rizqika Mulia Pratama_ITB/gdscday1.py
@@ -0,0 +1,4 @@
+print("Nama : Rizqika Mulia Pratama")
+print("Jurusan : STEI-K")
+print("Universitas : Institut Teknologi Bandung (ITB)")
+print("Alasan ingin masuk GDSC : Ingin belajar banyak mengenai dunia IT dan memperluas network dan pengalaman")
