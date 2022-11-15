6ba95e342ab6e9cd149cc932c445350a8731e683
unknown <ammarrnfl@Gmail.com>
2022-11-15 14:56:13 +0700

    First submission by Ammar Naufal

---
+++ Ammar Naufal_ITB/main.py
@@ -0,0 +1,4 @@
+print("Nama : Ammar Naufal")
+print("Jurusan : TPB")  
+print("Universitas : Institut Teknologi Bandung")
+print("Alasan ingin masuk GDSC : Ingin mencoba hal baru dan mendalami bidang pemrograman")
