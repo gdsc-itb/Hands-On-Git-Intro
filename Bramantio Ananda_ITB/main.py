c267935ae1fcbef9ac23168d0e3f1b2c67c9fbba
23Senna <117981449+23Senna@users.noreply.github.com>
2022-11-16 23:17:39 +0700

    First submission by Bramantio Ananda

---
+++ Bramantio Ananda_ITB/main.py
@@ -0,0 +1,4 @@
+print("Nama: Bramantio Ananda")
+print("Jurusan: TPB STEI-R ITB")
+print("Universitas: ITB")
+print("Alasan ingin masuk GDSC: Mengembangkan skill programming dan bekerja serta belajar bersama teman-teman dengan minat yang sama")
