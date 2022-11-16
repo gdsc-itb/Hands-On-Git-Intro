94d1e3cb99afddf6f8efa06f83f66067069a8966
Michelle Angela Aryanto <michelle.aryanto@binus.ac.id>
2022-11-16 18:43:34 +0700

    First Submission by Michelle Angela Aryanto

---
+++ Michelle Angela Aryanto_Binus/Identitas.md
@@ -0,0 +1,4 @@
+# First Submission by Michelle Angela Aryanto
+
+## Screenshot
+![Screenshot](screenshotMichelle.png)
---
+++ Michelle Angela Aryanto_Binus/main.py
@@ -0,0 +1,4 @@
+print("Nama : Michelle Angela Aryanto")
+print("Jurusan : Computer Science")
+print("Universitas : Binus University")
+print("Alasan ingin masuk GSDC : Saya ingin menjadi bagian dari sebuah komunitas yang impactful, seperti Google selama menjalani kehidupan sebagai calon Software Developer sehingga dapat menghasilkan karya dan solusi teknologi.")
