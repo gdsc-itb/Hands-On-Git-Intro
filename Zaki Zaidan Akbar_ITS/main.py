97627ed78184217f3aeeb0abdc259811ac45202f
Budiono55 <48086229+Budiono55@users.noreply.github.com>
2022-11-16 22:57:27 +0700

    Change The Desc Inside

---
+++ Zaki Zaidan Akbar_ITS/main.py
@@ -0,0 +1,4 @@
+print("Nama: Zaki Zaidan Akbar")
+print("Jurusan: Teknik Mesin")
+print("Universitas: ITS")
+print("Alasan ingin masuk GDSC: IoT, GUI, dan Data butuh saya pelajari untuk membuat projek kedepannya")
