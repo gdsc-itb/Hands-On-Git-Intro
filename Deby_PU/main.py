3c93385afb166aa03fe1bb4426472b06f3c38405
ayudebi52@gmail.com <ayudebi52@gmail.com>
2022-11-16 10:24:49 +0700

    First submission by Deby Ayu

---
+++ Deby_PU/main.py
@@ -0,0 +1,4 @@
+print("Nama                    : Deby Ayu Putri B")
+print("Jurusan                 : Sistem Informasi")
+print("Universitas             : President University")
+print("Alasan ingin masuk GDSC : ingin menambah skill dan belajar tentang computer science")
