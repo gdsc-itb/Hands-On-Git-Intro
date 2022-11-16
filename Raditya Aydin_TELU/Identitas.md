66d1de3015e6a1affbd4264af3c67ecc5d0980dd
Radiit <radityaaydin@gmail.com>
2022-11-16 11:03:59 +0700

    markdown file

---
+++ Raditya Aydin_TELU/Identitas.md
@@ -0,0 +1,4 @@
+# First submission by Raditya Aydin
+
+## Screenshot
+![Screenshot](ss GDSC 1.png)
---
+++ Raditya Aydin_TELU/main.py
@@ -0,0 +1,4 @@
+print("Nama: Raditya Aydin")
+print("Jurusan: Informatika")
+print("Universitas: Telkom University")
+print("Alasan ingin masuk GDSC: Pengen jago secepatnya")
