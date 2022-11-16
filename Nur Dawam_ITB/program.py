4d25fb7e3de52ed3fb3a692f495bfcd625ab768d
nur2m4 <mawadrun@gmail.com>
2022-11-16 14:23:02 +0700

    First submission by Nur Dawam

---
+++ Nur Dawam_ITB/program.py
@@ -0,0 +1,4 @@
+print("Nama : Nur Dawam Abdan Syakuro")
+print("Jurusan : STEI-R")
+print("Universitas : ITB")
+print("Alasan ingin masuk GDSC : Menyukai komputer, pemrograman, dan ingin mempelajari lebih dalam.")
