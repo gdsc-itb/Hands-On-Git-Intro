8bca86bcaddb06675aa28e69912c36e586dd1bf2
Bryan Cornelius Lauwrence <bryan.cornelius.lauw@gmail.com>
2022-11-16 23:03:24 +0700

    First submission by Bryan Cornelius Lauwrence

---
+++ Bryan Cornelius Lauwrence_ITB/main.py
@@ -0,0 +1,4 @@
+print ("Nama : Bryan Cornelius Lauwrence")
+print ("Jurusan : STEI-K")
+print ("Universitas : ITB")
+print ("Alasan ingin masuk GDSC : Ingin mendapatkan ilmu dan pengalaman dalam pemrograman dan mendapatkan teman baru.")
