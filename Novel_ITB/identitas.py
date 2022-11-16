a1fe631f9afb58db11b5365d9a8a2064397f2c0b
novelxv <novelyaputriramadhani11@gmail.com>
2022-11-16 16:00:24 +0700

    First submission by Novelya Putri Ramadhani

---
+++ Novel_ITB/identitas.py
@@ -0,0 +1,4 @@
+print("Nama : Novelya Putri Ramadhani")
+print("Jurusan : TPB STEI-K")
+print("Universitas : Institut Teknologi Bandung")
+print("Alasan ingin masuk GDSC : Ingin menambah skill di bidang pemrograman")
