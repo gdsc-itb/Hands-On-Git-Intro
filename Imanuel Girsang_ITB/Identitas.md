fa951e30d3d00e5fe534411bc3a5aadd799b62f8
ImanuelGirsang <71023099+ImanuelSG@users.noreply.github.com>
2022-11-16 15:48:12 +0700

    First submission by Imanuel Girsang

---
+++ Imanuel Girsang_ITB/Identitas.md
@@ -0,0 +1,4 @@
+# First submission by Imanuel Girsang
+
+## Screenshot
+![Screenshot](screenshot.png)
---
+++ Imanuel Girsang_ITB/main.py
@@ -0,0 +1,4 @@
+print("Nama: Imanuel Sebastian Girsang")
+print("Jurusan: TPB STEI-K")
+print("Universitas: ITB")
+print("Alasan ingin masuk GDSC: Memperluas pengetahuan di bidang teknologi dan bertemu dengan orang-orang lain yang memiliki minat sejenis")
