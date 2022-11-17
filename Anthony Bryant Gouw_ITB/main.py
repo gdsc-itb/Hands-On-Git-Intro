03a8434b151a53a75f79b5f7acebe716af7e6a78
Bryantgouw <bryantgouw2018@gmail.com>
2022-11-18 06:56:06 +0700

    First submission by Anthony Bryant Gouw

---
+++ Anthony Bryant Gouw_ITB/main.py
@@ -0,0 +1,4 @@
+print("Nama : Anthony Bryant Gouw")
+print("Jurusan : STEI-K")
+print("Universitas : Institut Teknologi Bandung (ITB)")
+print("Alasan ingin masuk GDSC : ketertarikan di bidang teknologi dan coding, networking, dan develop new skills")
