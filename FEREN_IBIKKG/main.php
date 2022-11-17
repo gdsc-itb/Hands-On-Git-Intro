882524b270cdb11e52b20f127ca474c3efb10618
feren <ferentambunan@gmail.com>
2022-11-18 04:41:53 +0700

    First submission by FEREN

---
+++ FEREN_IBIKKG/main.php
@@ -0,0 +1,8 @@
+<?php
+
+
+
+echo "Nama: Feren Cristina T\n";
+echo "Jurusan: Teknik Informatika\n";
+echo "Universitas: IBIKKG\n";
+echo "Alasan ingin masuk GDSC: Menambah pengetahuan dan relasi\n";
