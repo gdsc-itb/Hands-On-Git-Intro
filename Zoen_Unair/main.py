915ef8f5564ab03830bf351647af53f7c84ef395
zoenys <sianiparzoen@gmail.com>
2022-11-17 16:51:34 +0700

    First submission by Zoen

---
+++ Zoen_Unair/main.py
@@ -0,0 +1,4 @@
+print("Nama: Zoen Yokhanan Sianipar")
+print("Jurusan: Teknologi Sains Data")
+print("Universitas: Universitas Airlangga")
+print("Alasan ingin masuk GDSC: Menambah insight dan mencari teman baru yang punya ketertarikan bidang data")
