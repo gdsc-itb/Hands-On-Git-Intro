1613c787fb07b78f2b0ad37205b8b07ec168dbfb
Firly Hafiz Syahreza <firlyhafizsyahreza42@gmail.com>
2022-11-17 18:34:36 +0700

    First submission by Firly Hafiz Syahreza

---
+++ Firly Hafiz Syahreza_ITB/program.py
@@ -0,0 +1,4 @@
+print("Nama : Firly Hafiz Syahreza")
+print("Jurusan : FITB")
+print("Universitas : Institut Teknologi Bandung")
+print("Alasan ingin masuk GDSC : Ingin belajar mengenai konsep dan praktikal dari teknologi komputasi")
