97d1a9824d2724430224dd1e374624c8ea8b0089
denoseu <denisefeliciatiowanni@gmail.com>
2022-11-17 22:29:45 +0700

    First submission by Ignatius Jhon Hezkiel Chan

---
+++ Kiel Chan_ITB/main.py
@@ -0,0 +1,4 @@
+print("Nama: Ignatius Jhon Hezkiel Chan")
+print("Jurusan: STEI - Komputasi")
+print("Universitas: ITB")
+print("Alasan ingin masuk GDSC: Saya mau belajar lebih banyak tentang pemrograman and I simply just love Google.")
