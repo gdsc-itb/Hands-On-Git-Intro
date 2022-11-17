383054a9411ef94ad627a42c92dcaee48bbce33d
haidar <89732397+haidarhamda@users.noreply.github.com>
2022-11-17 20:34:45 +0700

    First submission by Haidar Hamda

---
+++ Haidar Hamda_ITB/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    printf("Nama: Haidar Hamda\n");
+    printf("Jurusan: Teknik Informatika\n");
+    printf("Universitas: ITB\n");
+    printf("Alasan ingin masuk GDSC: Ingin belajar lebih banyak tentang pemrograman\n");
+    return 0;
+}
