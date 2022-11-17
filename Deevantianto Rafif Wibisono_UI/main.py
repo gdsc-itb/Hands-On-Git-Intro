e8bb06ee55689f69dbb0db14076ffd7161f04e3a
deevanwibisono <deevanwibisono@gmail.com>
2022-11-17 19:40:02 +0700

    First Submission by Deevantianto Rafif Wibisono

---
+++ Deevantianto Rafif Wibisono_UI/main.py
@@ -0,0 +1,4 @@
+print("Nama : Deevantianto Rafif Wibisono ")
+print("Jurusan : Teknik Industri")
+print("Universitas : Universitas Indonesia")
+print("Alasan ingin masuk GDSC : Ingin bertemu dengan teman-teman dari kampus lain dan belajar data sainss/machine learning")
