f37ab9bb7bd20657d834f4c1b06ea0119b1e371f
yg-firnanda <mochyoga475@gmail.com>
2022-11-17 14:27:51 +0700

    First submission by Mochammad Yoga Firnanda

---
+++ Mochammad Yoga Firnanda_UPN Jatim/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Nama : Mochammad Yoga Firnanda\n";
+    cout << "Jurusan : Informatika\n";
+    cout << "Universitas : UPN 'Veteran' Jawa Timur\n";
+    cout << "Alasan ingin masuk GDSC : Saya ingin belajar lebih banyak tentang teknologi.";
+}
