bfefc4ecadaf8a2bad3cb55c84bc50d426cd129d
monicaangelah <monicaangelahartono@gmail.com>
2022-11-17 09:52:55 +0700

    First submission by Monica

---
+++ Monica Angela Hartono_ITB/Identitas.md
@@ -0,0 +1,4 @@
+# First submission by Monica Angela Hartono
+
+## Screenshot
+![Screenshot](screenshot.png)
---
+++ Monica Angela Hartono_ITB/main.py
@@ -0,0 +1,4 @@
+print("Nama: Monica Angela Hartono")
+print("Fakultas: STEI-K")
+print("Universitas: ITB")
+print("Alasan ingin masuk GDSC: Ingin belajar kebih lagi tentang pemrograman, menambah relasi dengan orang-orang yang memiliki ketertarikan yang sama dengan saya, mendapatkan partner lomba untuk bersama-sama berkembang.")
