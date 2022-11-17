5296db14bbb9f47c6fe544b1f12c1e3e72aa36f9
Chiko but no H <franciscotrianto@gmail.com>
2022-11-17 20:28:48 +0700

    First submission by Raden Francisco Trianto Bratadiningrat

---
+++ Raden Francisco Trianto Bratadiningrat_ITB/Identitas.md
@@ -0,0 +1,4 @@
+# First submission by Raden Francisco Trianto Bratadiningrat
+
+## Screenshot
+![Screenshot](screenshot.png)
---
+++ Raden Francisco Trianto Bratadiningrat_ITB/main.py
@@ -0,0 +1,6 @@
+print("""
+Nama : Raden Francisco Trianto Bratadiningrat 
+Jurusan : STEI-K
+Universitas : ITB
+Alasan ingin masuk GDSC : Explore lebih dalam dunia IT/CS serta mencari ilmu baru mengenai carier di bidang IT/CS
+""")
