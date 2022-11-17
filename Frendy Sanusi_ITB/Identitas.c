549b1f0d9e788e930375ab74d2d0212408952c16
frendysanusi05 <frendyvivo5@gmail.com>
2022-11-17 21:03:40 +0700

    First submission by Frendy Sanusi

---
+++ Frendy Sanusi_ITB/Identitas.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    char Nama[] = "Frendy Sanusi";
+    char Jurusan[] = "Sistem dan Teknologi Informasi";
+    char Universitas[] = "Institut Teknologi Bandung";
+    char Alasan[] = "Ingin menambah relasi serta pengetahuan mengenai teknologi terutama berbasis Google";
+
+    printf("Nama : %s\nJurusan : %s\nUniversitas : %s\nAlasan ingin masuk GDSC : %s\n", Nama, Jurusan, Universitas, Alasan);
+}
