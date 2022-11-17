999e57e454f3fa5fe4f49a85c80e96a6ce78e2af
unknown <you@example.com>
2022-11-17 18:39:19 +0700

    First submission by Alif Aqila Hakim

---
+++ AlifAqilaHakim_ITB/main0.py
@@ -0,0 +1,4 @@
+print("Nama: Alif Aqila Hakim")
+print("Jurusan: FTI peminatan Teknik Bioenergi")
+print("Universitas: ITB")
+print("Alasan ingin masuk GDSC: Ingin belajar lebih banyak tentang pemrograman")
