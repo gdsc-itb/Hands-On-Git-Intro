9ea59b528d3e8c88bd19c03c2c0b398939726ea3
unknown <heidischan01@gmail.com>
2022-11-19 01:00:02 +0700

    First submission by Heidi Schan

---
+++ Heidi Schan_ITB/main.py
@@ -0,0 +1,4 @@
+print("Nama: Heidi Schan Andriana")
+print("Jurusan: STEI-R")
+print("Universitas: ITB")
+print("Alasan ingin masuk GDSC: Ingin mendalami ilmu teknologi yang dapat diterapkan di pembelajaran kuliah dan dunia kerja serta mengikuti kompetisi yang sesuai dengan minat dan bakat di bidang teknologi")
