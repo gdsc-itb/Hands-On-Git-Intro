97fb56aa4a98dc41056e9b27672d2f30cc11d37e
firsaaa <firsa.raissa@gmail.com>
2022-11-18 20:50:24 +0700

    First submission by Firsa Athaya Raissa Alifah

---
+++ Firsa Athaya Raissa Alifah_ITB/main.py
@@ -0,0 +1,4 @@
+print("Nama: Firsa Athaya Raissa Alifah")
+print("Jurusan: STEI-K")
+print("Universitas: ITB")
+print("Alasan ingin masuk GDSC: Ingin menambah ilmu tentang pemrograman dan bertemu serta belajar dengan teman-teman dengan minat yang sama")
