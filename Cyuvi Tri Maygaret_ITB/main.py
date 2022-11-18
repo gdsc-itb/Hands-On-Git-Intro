9cd0305bc46ce6e3617d74a35bf9c4d4380f1827
Maycyuvi <cyuvi_tri@sbm-itb.ac.id>
2022-11-18 22:01:28 +0700

    Fisrt submission by Cyuvi Tri Maygaret

---
+++ Cyuvi Tri Maygaret_ITB/main.py
@@ -0,0 +1,4 @@
+print("Nama : Cyuvi Tri Maygaret")
+print("Jurusan : SBM")
+print("Universitas : Institut Teknologi Bandung")
+print("Alasan ingin masuk GDSC : Ingin mempelajari skill coding dengan para master")
