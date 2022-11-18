36cc8aa3098aef0e4d30dfd489f613fc06ace045
FaiqJabbar <91514052+FaiqJabbar@users.noreply.github.com>
2022-11-18 14:16:18 +0700

    Update main.py

--- Faiq_Jabbar_TELU/main.py
+++ Faiq_Jabbar_TELU/main.py
@@ -1,4 +1,4 @@
 print("Nama: Muhammad Faiq Jabbar")
-print("Jurusan: Informatika")
+print("Jurusan: S1 Informatika")
 print("Universitas: Telkom University")
 print("Alasan ingin masuk GDSC: Ingin menambah relasi dan juga menambah ilmu saya dalam perkodingan")
