013abd66be5cb8b2dce77582c62296c34d05ab80
Tamara <taralubis12@gmail.com>
2022-11-18 07:27:50 +0700

    First submission by Tamara Mayranda Lubis

---
+++ Tamara Mayranda Lubis_ITB/main.py
@@ -0,0 +1,4 @@
+print("Nama: Tamara Mayranda Lubis")
+print("Jurusan: STEI-Komputasi (TPB)")
+print("Universitas: ITB")
+print("Alasan ingin masuk GDSC: Ingin menambah skill terutama dalam bidang teknologi dan mencari relasi")
