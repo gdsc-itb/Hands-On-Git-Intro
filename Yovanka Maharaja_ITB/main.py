72c92e28d3234aed7895fdb82c6416155f2618b4
yovankas <yovankaasm@gmail.com>
2022-11-18 13:25:04 +0700

    First Submission by Yovanka Maharaja

---
+++ Yovanka Maharaja_ITB/main.py
@@ -0,0 +1,4 @@
+print ("Nama : Yovanka Sandrina Maharaja")
+print ("Jurusan : TPB Sekolah Teknik Elektro dan Informatika")
+print ("Universitas : ITB")
+print ("Alasan ingin masuk GDSC : Ingin mendalami dan belajar lebih dalam tentang pemrograman, terutama data science dan UI/UX design, serta memperluas relasi")
