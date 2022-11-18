a9a6cebda1f7bb2939c7d64fa001d8d944edf6b5
MRafliRasyiidin <masraflirasid@gmail.com>
2022-11-18 13:01:41 +0700

    First submission by Muhamad Rafli Rasyiidin

---
+++ Muhamad Rafli_ITB/Identitas.md
@@ -0,0 +1,4 @@
+# First Submission by Muhamad Rafli Rasyiidin
+
+# Screenshot
+![Screenshot](screenshot.png)
---
+++ Muhamad Rafli_ITB/Tugas GDSC 1.py
@@ -0,0 +1,7 @@
+
+print('''
+Nama                    : Muhamad Rafli Rasyiidin
+Fakultas                : STEI-K
+Universitas             : Institut Teknologi Bandung
+Alasan ingin masuk GDSC : Ingin meningkatkan kemampuan programming dan belajar hal baru
+''')
