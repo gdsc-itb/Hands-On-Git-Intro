a7899f7e819d1276dae8438c7159fcf089041a69
Loxenary <110291644+Loxenary@users.noreply.github.com>
2022-11-18 15:16:35 +0700

    First submission by Muhammad Davis

---
+++ Muhammad Davis_ITB/Tugas GDSC 1.py
@@ -0,0 +1,7 @@
+print('''
+Nama : Muhammad Davis Adhipramana 
+Jurusan : STEI-TPB
+Universitas : ITB 
+Alasan ingin masuk GDSC : Saya ingin melakukan eksplorasi lebih lanjut untuk mengenai bidang tentang hal tertentu yang cocok
+untuk saya, saya ingin mencoba memelajari ui/front-end web/machine learning yang sekirannya akan cocok dengan saya. 
+''')
