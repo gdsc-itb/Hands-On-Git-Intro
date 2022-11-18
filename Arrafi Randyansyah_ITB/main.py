6d475a95df3c4c5ba9921a760a707b3705fac48c
unknown <ajokeren>
2022-11-18 22:35:14 +0700

    First submission by Arrafi RAndyansyah

---
+++ Arrafi Randyansyah_ITB/main.py
@@ -0,0 +1,6 @@
+print('Nama: Arrafi Randyansyah Faizi')
+print('Fakultas: FMIPA (jurusan: Kimia')
+print('Universitas: ITB')
+print('Alasan ingin masuk GDSC: Tadinya karena FOMO,')
+print('tapi saya mencoba ngeksplor ternyata seru juga.')
+print('Sekarang tujuan saya masuk GDSC untuk bisa ahli dibidang Kimia Komputasi')
