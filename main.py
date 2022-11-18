dbca0f4d385eff3dd17127626afb4cca4f2bada9
b33rk <117975143+b33rk@users.noreply.github.com>
2022-11-18 22:14:50 +0700

    Delete main.py

--- main.py
+++
@@ -1,4 +0,0 @@
-print("Nama: Abdullah Mubarak")
-print("Jurusan: STEI-K")
-print("Universitas: ITB")
-print("Alasan ingin masuk GDSC: Ingin meningkatkan skill dan pengetahuan mengenai programming, khususunya AI dan data science, menambah relasi")
