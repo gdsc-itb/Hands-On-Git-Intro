014836d7d812d4271209f3a73adb77c7420fad0b
Ardra15 <ardrarafif1505@gmail.com>
2022-11-18 22:37:00 +0700

    First submission by Ardra

---
+++ Ardra Rafif Sahasika_ITB/index.py
@@ -0,0 +1,4 @@
+print("Nama: Ardra Rafif Sahasika")
+print("Jurusan: TPB")
+print("Universitas: ITB")
+print("Alasan ingin masuk GDSC: Aku ingin menguasai data analysis, membuat sebuah AI, dan menguasai full stack web developer. Selain itu aku ingin bertemu teman-teman yang berguna untuk meningkatkan relasi")
