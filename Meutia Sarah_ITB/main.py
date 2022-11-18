aa60d72cc59354a25332bb2bb045aff8e606792f
Meutia Sarah <meutia.sarahnr@gmail.com>
2022-11-18 18:21:50 +0700

    First submission by Meutia Sarah Nur Raisha

---
+++ Meutia Sarah_ITB/main.py
@@ -0,0 +1,4 @@
+print("Nama: Meutia Sarah Nur Raisha")
+print("Jurusan: STEI-R")
+print("Universitas: ITB")
+print("Alasan ingin masuk GDSC: Menambah wawasan tentang programming serta menambah relasi")
