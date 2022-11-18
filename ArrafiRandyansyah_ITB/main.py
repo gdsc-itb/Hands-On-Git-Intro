5b917a3a04dcfcbc930a62689e3e5fecde2cfac9
ajokeren <118087366+ajokeren@users.noreply.github.com>
2022-11-18 22:30:22 +0700

    Delete main.py

--- ArrafiRandyansyah_ITB/main.py
+++
@@ -1,6 +0,0 @@
-print('Nama: Arrafi Randyansyah Faizi')
-print('Fakultas: FMIPA (jurusan: Kimia')
-print('Universitas: ITB')
-print('Alasan ingin masuk GDSC: Tadinya karena FOMO,')
-print('tapi saya mencoba ngeksplor ternyata seru juga.')
-print('Sekarang tujuan saya masuk GDSC untuk bisa ahli dibidang Kimia Komputasi')
