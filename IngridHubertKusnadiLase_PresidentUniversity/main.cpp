ece54b8328350edfbbdce4a43ab3e41909e057e1
IngridHubertKL <ingridhubert99@gmail.com>
2022-11-18 17:09:14 +0700

    First submission by Ingrid Hubert Kusnadi Lase

---
+++ IngridHubertKusnadiLase_PresidentUniversity/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Name: Ingrid Hubert Kusnadi Lase\n";
+  cout << "Jurusan: Information system\n";
+  cout << "Universitas: President University\n";
+  cout << "Alasan ingin masuk GDSC: I want to develop my interests around the world of programming\n";
+  return 0;
+}
