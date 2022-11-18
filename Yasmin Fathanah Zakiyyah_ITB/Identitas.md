807c5692c596cee4ab037558874f02b8b66a9e05
Yasmin Fathanah Zakiyyah <yasminfath@upi.edu>
2022-11-18 20:31:09 +0700

    First submission by Yasmin Fathanah Zakiyyah

---
+++ Yasmin Fathanah Zakiyyah_ITB/Identitas.md
@@ -0,0 +1,4 @@
+# First submission by Yasmin Fathanah Zakiyyah
+
+## Screenshot
+![Screenshot](screenshot.png)
---
+++ Yasmin Fathanah Zakiyyah_ITB/main.py
@@ -0,0 +1,4 @@
+print("Nama : Yasmin Fathanah Zakiyyah")
+print("Jurusan : TPB STEI-R")
+print("Universitas : Institut Teknologi Bandung")
+print("Alasan ingin masuk GDSC : Mengembangkan pengetahuan dan memperluas relasi di bidang teknologi")
