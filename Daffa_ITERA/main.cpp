bcb3fc108a20b8b8e28f9dca23e6c3dbd9f20bff
DaffaJatmiko <daffamiko123@gmail.com>
2022-11-18 17:05:02 +0700

    First submission by Daffa Abdurrahman Jatmiko

---
+++ Daffa_ITERA/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Nama : Daffa Abdurrahman Jatmiko" << endl;
+  cout << "Jurusan : Teknik Informatika" << endl;
+  cout << "Universitas : Institut Teknologi Sumatera " << endl;
+  cout << "Alasan ingin masuk GDSC : Karena ingin memperdalam ilmu tentang web programming " << endl;
+}
