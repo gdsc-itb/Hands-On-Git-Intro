f3c1f94b61d5dbdb0585ffa03bc2188beed1f251
ZeevHaydar <nzhifhaidar@gmail.com>
2022-11-18 16:49:25 +0700

    First Submission by Nazhif Haidar

---
+++ Nazhif Haidar_ITB/main_gdsc.py
@@ -0,0 +1,2 @@
+print("Nama: Nazhif Haidar Putra Wibowo\nJurusan: Sistem dan Teknologi Informasi\nUniversitas: Institut Teknologi Bandung")
+print("Alasan ingin masuk GDSC: Karena ingin menambah pengetahuan dan skill IT dan programming.")
