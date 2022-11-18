d946b09f6f30fb193fb3f0ca8af4933861ed4c03
yosef <yosefnuragaw@gmail.com>
2022-11-18 17:42:10 +0700

    First submission by Yosef Nuraga Wicaksana

---
+++ Yosef Nuraga Wicaksana_UI/main.py
@@ -0,0 +1,4 @@
+print("Nama: Yosef Nuraga Wicaksana")
+print("Jurusan: Sistem Informasi")
+print("Universitas: UI")
+print("Alasan ingin masuk GDSC: Memperbanyak relasi dan mendalami dunia programming")
