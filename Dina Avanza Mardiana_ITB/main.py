538aaf3c52e8a0616bf565331124404a22181f31
dinxx23 <dinaavanzamardiana@gmail.com>
2022-11-18 08:00:50 +0700

    first submission by Dina Avanza Mardiana

---
+++ Dina Avanza Mardiana_ITB/main.py
@@ -0,0 +1,4 @@
+print("Nama : Dina Avanza Mardiana")
+print("Jurusan : SITH-S")
+print("Universitas : Institut Teknologi Bandung")
+print("Alasan ingin masuk GDSC : Ingin belajar dasar-dasar programming dan ingin memperdalam tentang bioinformatika")
