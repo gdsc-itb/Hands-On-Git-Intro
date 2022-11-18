ea1fa62fb8df758f0f73d26b47ae8b433e1f5860
natthankrish <92136335+natthankrish@users.noreply.github.com>
2022-11-18 23:33:51 +0700

    First submission by Antonio Natthan Krishna

---
+++ Antonio Natthan Krishna_ITB/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main () {
+    printf("Nama : Antonio Natthan Krishna\n");
+    printf("Jurusan : Teknik Informatika\n");
+    printf("Universitas: Institut Teknologi Bandung\n");
+    printf("Alasan ingin masuk GDSC: ingin menambah skill web-development\n");
+    return 0;
+}
