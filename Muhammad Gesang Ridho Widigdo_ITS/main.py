bafbc29458332ca348ca0f7cbe17ca10ad1fd1bd
Rein <54650525+gesangwidigdo@users.noreply.github.com>
2022-11-18 14:07:17 +0700

    First submission by Muhammad Gesang Ridho Widigdo

---
+++ Muhammad Gesang Ridho Widigdo_ITS/main.py
@@ -0,0 +1,4 @@
+print("Nama: Muhammad Gesang Ridho Widigdo")
+print("Jurusan: Teknik Informatika")
+print("Universitas: ITS")
+print("Alasan ingin masuk GDSC: Ingin belajar mengenai Software Engineering dan belajar mengenai jenis-jenis software developer untuk mengetahui jenis developer yang cocok untuk saya di masa depan.")
