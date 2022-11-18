db160b676dc3c32671606daceadaa8146c4655d9
Sherry-21 <antonio.ryanlie@binus.ac.id>
2022-11-18 23:07:22 +0700

    Program

---
+++ code.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define lets using
+#define go namespace
+#define icpc std
+#define memset(a,b) memset(a, b, sizeof(a));
+#define ll long long
+#define ull unsigned long long int
+#define print(a) for(auto i : a)cout<<i<<"\n";
+#define loop(n) for(int i = 0;i<n;i++)
+#define loop2(n) for(int j = 0;j<n;j++)
+
+lets go icpc;
+
+int main(){
+  cout << "Nama : Antonio Ryanlie" << endl;
+  cout << "Jurusan : Computer Science" << endl;
+  cout << "Universitas : Universitas Bina Nusantara" << endl;
+  cout << "Alasan ingin masuk GDSC : Ingin menambah relasi serta pengetahuan seputar dunia teknologi" << endl;
+  return 0;
+}
