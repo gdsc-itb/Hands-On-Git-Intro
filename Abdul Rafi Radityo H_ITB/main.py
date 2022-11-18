7ece831f55eb5dfeecd49a0833a384c6c304b113
lifepain <abdulrafi623@gmail.com>
2022-11-18 23:15:37 +0700

    First submission by Abdul Rafi Radityo Hutomo

---
+++ Abdul Rafi Radityo H_ITB/main.py
@@ -0,0 +1,4 @@
+print("Nama : Abdul Rafi Radityo Hutomo")
+print("Fakultas : STEI-K")
+print("Universitas : Institut Teknologi Bandung")
+print("Alasan ingin masuk GDSC : Untuk menambah kemampuan dan wawasan dalam bidang IT.")
