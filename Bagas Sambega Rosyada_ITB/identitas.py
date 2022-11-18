bd3b1914d9ace2adef6c01c71175578ca5329f2b
bagassambega <bagassambega@gmail.com>
2022-11-18 18:51:42 +0700

    First submission by Bagas Sambega Rosyada

---
+++ Bagas Sambega Rosyada_ITB/identitas.py
@@ -0,0 +1,5 @@
+# IDENTITAS
+print("Nama : Bagas Sambega Rosyada")
+print("Jurusan : Sekolah Teknik Informatika dan Komputasi (TPB)")
+print("Universitas : Institut Teknologi Bandung")
+print("Alasan ingin masuk GDSC : Saya ingin mengembangkan dan meningkatkan wawasan juga skill saya dalam bidang teknologi komputasi. Saya juga ingin mengasah kemampuan saya dalam berorganisasi dan berkomunikasi, juga mengenal lebih dekat komunitas Google di Institut Teknologi Bandung")
