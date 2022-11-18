4386b3a0a0d42360296650c00e311fbdbaa5f38a
StayHalal01 <irfanridhana@gmail.com>
2022-11-18 16:34:28 +0700

    First submission by Irfan Ridhana

---
+++ Irfan Ridhana_ITS/main.py
@@ -0,0 +1,4 @@
+print("Nama: Irfan Ridhana")
+print("Jurusan: Teknik Informatika")
+print("Universitas: ITS")
+print("Alasan ingin masuk GDSC: Saya ingin menjadi member GDSC ITB karena saya ingin mengembangkan ilmu teknologi dan programming serta komputasional saya lebih dalam. Selain itu saya ingin mencoba mencari ilmu di luar kampus dan memperluas relasi.")
