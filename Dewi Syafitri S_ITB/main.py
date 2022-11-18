34593647dacc29ab3e4808c6c58f8105c2bf1e3f
Dewi <dewi.syarf@gmail.com>
2022-11-19 00:07:40 +0700

    First Submission by Dewi Syafitri S

---
+++ Dewi Syafitri S_ITB/main.py
@@ -0,0 +1,4 @@
+print("Nama: Dewi Syafitri")
+print("Jurusan: TPB STEI")
+print("Universitas: ITB")
+print("Alasan ingin masuk GDSC: Ingin mempelajari hal baru dan belajar lebih dalam mengenai pemograman")
