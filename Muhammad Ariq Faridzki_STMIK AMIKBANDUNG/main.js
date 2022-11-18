da28b766ee462816898ad6ee74a63197aa05c336
AriqFaridzki <ariqf35@gmail.com>
2022-11-18 14:43:07 +0700

    First Submission by Muhammad Ariq

---
+++ Muhammad Ariq Faridzki_STMIK AMIKBANDUNG/main.js
@@ -0,0 +1,6 @@
+console.log("Nama : Muhammad Ariq Faridzki");
+console.log("Jurusan : Teknik Informatika");
+console.log("Universitas : STMIK 'AMIKBANDUNG'");
+console.log(
+  "Alasan ingin masuk GDSC : Ingin meningkatkan skill dan Berkontribusi untuk masyarakat"
+);
