b73354547c82619886c21709424311f6e7f9b2eb
FairnaMustika <fairnafafa11@gmail.com>
2022-11-18 22:00:54 +0700

    First Submission by Fairna

---
+++ Fairna Mustika_ITS/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+printf("Nama: Fairna Mustika Hermafidhanti\n");
+printf("Jurusan: Teknik Informatika\n");
+printf("Universitas: ITS\n");
+printf("Alasan ingin masuk GDSC: Ingin belajar lebih banyak tentang pemrograman, UIUX, Webdev, serta mengembangkan diri di bidang informatika");
+}
