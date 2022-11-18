e46c9a816f034a9e133477d11c57125f4f8ce4ea
Adril Putra Merin <adribless37@gmail.com>
2022-11-18 16:53:40 +0700

    First submission by Adril Putra Merin

---
+++ Adril Putra Merin_ITB/main.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout<<"Nama : Adril Putra Merin"<<endl<<"Jurusan : STEI-Komputasi"<<endl;
+    cout<<"Universitas : Institut Teknologi Bandung"<<endl<<"Alasan ingin masuk GDSC : Ingin mengembangkan skill di bidang programming serta mencari koneksi dan komunitas dengan minat yang sama";
+}
