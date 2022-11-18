baa2a008fb7a4d7cd6dfea41c456cacb97437c5c
Rosiana Amelia Sari Bangun <ocibangun@gmail.com>
2022-11-18 13:37:57 +0700

    First SUbmiss

--- Rosiana Amelia_ITB/main.py
+++ Rosiana Amelia_ITB/main.py
@@ -2,3 +2,9 @@ print("Nama : Rosiana Amelia Sari Bangun")
 print("Jurusan : TPB - Fakultas Teknologi Industri")
 print("Universitas : ITB")
 print("Alasan ingin masuk GDSC : Karena saya suka programming <33 ")
+
+
+print("Nama: Malik Akbar")
+print("Jurusan: Teknik Informatika")
+print("Universitas: ITB")
+print("Alasan ingin masuk GDSC: Ingin belajar lebih banyak tentang pemrograman")
