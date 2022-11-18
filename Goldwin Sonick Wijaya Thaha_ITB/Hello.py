07fd85862168010efebd96b9865def35d93046b0
gswt <goldwinsonick93@gmail.com>
2022-11-18 21:14:25 +0700

    First submission by Goldwin Sonick Wijaya Thaha

---
+++ Goldwin Sonick Wijaya Thaha_ITB/Hello.py
@@ -0,0 +1,6 @@
+print("""
+Nama : Goldwin Sonick Wijaya Thaha
+Jurusan : STEI-R 
+Universitas : ITB
+Alasan ingin masuk GDSC : Ingin belajar hal baru dan membangun koneksi dengan teman yang minatnya sama
+""")
