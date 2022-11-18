4d080e1de5fed8730ad2566ca0c4b20d858ca7c5
nathania01 <nathania.calista01@gmail.com>
2022-11-18 20:28:48 +0700

    First Submission by Nathania Calista

---
+++ Nathania Calista_ITB/main.py
@@ -0,0 +1,4 @@
+print("Nama: Nathania Calista Djunaedi")
+print("Jurusan: Teknik Informatika")
+print("Universitas: ITB")
+print("Alasan ingin masuk GDSC: Ingin belajar tentang data science, webdev, atau software engineering dan mendapat banyak relas")
