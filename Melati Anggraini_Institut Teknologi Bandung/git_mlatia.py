1ec789ace2d5e8194cf3986307266618253162a6
mlatia <112107340+mlatia@users.noreply.github.com>
2022-11-18 23:34:02 +0700

    First submission by Melati Anggraini

---
+++ Melati Anggraini_Institut Teknologi Bandung/git_mlatia.py
@@ -0,0 +1,6 @@
+print('Nama : Melati Anggraini')
+print('Jurusan : STEI-K')
+print('Universitas : Institut Teknologi Bandung')
+print('Alasan ingin masuk GDSC : Ingin mengembangkan minat di dunia teknologi,', end="")
+print('terutama di bidang data science dan web development, serta menambah relasi', end='')
+print(' dengan orang-orang yang memiliki minat yang sama.')
