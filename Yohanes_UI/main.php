9ebba35df31cadc0a25ee5741624b5ae2ebbba93
JojoSitompul <117983357+JojoSitompul@users.noreply.github.com>
2022-11-18 14:21:52 +0700

    First submission by Yohanes

---
+++ Yohanes_UI/main.php
@@ -0,0 +1,6 @@
+<?php
+
+echo "Nama : Yohanes Jasillak Gavin Sitompul";
+echo "Jurusan : Teknik Industri";
+echo "Universitas : UI";
+echo "Alasan ingin masuk GDSC : Saya ingin mendalami dunia IT khususnya bidang pemrograman. GDSC ITB adalah salah satu klub untuk orang yang suka bergelud di bidang IT dan didalamnya ada banyak orang yang sudah berpengalaman juga. Sejujurnya saya mendapat info GDSC ITB dari post ig teman saya (anak STEI juga). Ya saya harap saya bisa belajar Data Science disini";
