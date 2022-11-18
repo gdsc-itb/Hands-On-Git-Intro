6dd367c3cda8e26e874a8e25e766373565ca1ab8
akmalhfzh <118372685+akmalhfzh@users.noreply.github.com>
2022-11-19 00:50:02 +0700

    First submission by < akmal hafizh >

--- Malik Akbar_ITB/main.py
+++ Malik Akbar_ITB/main.py
@@ -1,4 +1,4 @@
-print("Nama: Malik Akbar")
-print("Jurusan: Teknik Informatika")
+print("Nama: akmal hafizh")
+print("Jurusan: Tpb stei r")
 print("Universitas: ITB")
 print("Alasan ingin masuk GDSC: Ingin belajar lebih banyak tentang pemrograman")
