ef020e5cd3ff564c5bb0d215cfd7bc29af7ac527
zhuofan1412 <116002961+zhuofan1412@users.noreply.github.com>
2022-11-18 21:43:45 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Nama : Faiz Jauhar Azzam")
+print("Jurusan : Teknik Industri")
+print("Universitas : Institut Teknologi Bandung")
+print("Alasan masuk GDSC : Mengembangkan skill programming dan computational thinking")
