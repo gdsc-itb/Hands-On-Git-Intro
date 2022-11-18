e4c71f7477bd43287095aed015252584d3dbf5a0
SilverBudda <ikalesson15@gmail.com>
2022-11-18 22:20:44 +0700

    First submission by Erika Paula Tobing

---
+++ Erika Paula Tobing_PU/main.py
@@ -0,0 +1,4 @@
+print("Nama: Erika Paula Tobing\n")
+print("Jurusan: Infromation System\n")
+print("Universitas: President University\n")
+print("Alasan ingin masuk GDSC: I joined GDSC because I wanted to expand my abilities and widen my horizons in the field of creating mobile and web applications. I also wanted to increase my knowledge outside of campus\n")
