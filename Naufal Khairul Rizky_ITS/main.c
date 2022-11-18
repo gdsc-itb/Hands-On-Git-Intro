b79917feb9a193958f1fc18410241be36074c77e
Naufal Khairul <naufalkr394@gmail.com>
2022-11-18 09:09:40 +0700

    First submission by Naufal Khairul Rizky

---
+++ Naufal Khairul Rizky_ITS/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    printf("Nama: Naufal Khairul Rizky\n");
+    printf("Jurusan: Teknik Informatika\n");
+    printf("Universitas: ITS\n");
+    printf("Alasan ingin masuk GDSC: Untuk mengembangkan diri di bidang pemrograman dan ingin menambah relasi dengan sesama orang yang menggeluti di bidang yang sama\n");
+}
