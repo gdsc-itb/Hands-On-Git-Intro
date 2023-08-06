6c19e16bf21a1f39a819555f4afa31a6a16ffb7a
Thea <116005906+pandaandsushi@users.noreply.github.com>
2023-08-06 22:32:39 +0700

    Update main.py

--- Thea_ITB/main.py
+++ Thea_ITB/main.py
@@ -2,3 +2,5 @@ print("Nama: Thea Josephine Halim")
 print("Jurusan: TPB STEI-K")
 print("Universitas: ITB")
 print("Alasan ingin masuk GDSC: Ingin belajar lebih banyak tentang pemrograman, cari pengalaman baru, cari teman baru, dan menambah wawasan")
+
+print("test")
